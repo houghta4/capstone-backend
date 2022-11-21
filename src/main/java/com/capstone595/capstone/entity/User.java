a5f17c6d64c2879914fb1169ec592956b31e3c14
Vince <houghta4@uwm.edu>
2022-11-21 13:31:30 -0600

    init

---
+++ src/main/java/com/capstone595/capstone/entity/User.java
@@ -0,0 +1,91 @@
+package com.capstone595.capstone.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="users")     //needed because mysql user table is already used
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate cohortStartDate;
+    private String username;
+    private String password;
+
+
+    //TODO do we want a 2 way relationship? probably not
+//    private List<Authority> authorities;
+//
+//    public void setAuthorities(List<Authority> authorities) {
+//        this.authorities = authorities;
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCohortStartDate() {
+        return cohortStartDate;
+    }
+
+    public void setCohortStartDate(LocalDate cohortStartDate) {
+        this.cohortStartDate = cohortStartDate;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        //hard coded to user for now
+        roles.add(new Authority("ROLE_USER")); //ROLE_ is required
+        return roles;
+    }
+}
---
+++ src/main/java/com/capstone595/capstone/service/UserDetailsServiceImpl.java
@@ -0,0 +1,54 @@
+package com.capstone595.capstone.service;
+
+import com.capstone595.capstone.entity.User;
+import com.capstone595.capstone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOpt = userRepo.findByUsername(username); //findBy prefix is special with spring data. automatically figures out property
+
+//        return userOpt.orElseThrow(() -> new UsernameNotFoundException("Invalid credentials"));
+        return userOpt.orElse(null);
+    }
+
+    public User saveUser(User user){
+        return userRepo.save(user);
+    }
+
+    public User updateUser(User user) {
+        User existingUser = userRepo.findById(user.getId()).orElse(null);
+        if(existingUser != null){
+            existingUser.setPassword(user.getPassword());
+            existingUser.setUsername(user.getUsername());
+            existingUser.setCohortStartDate(user.getCohortStartDate());
+        }
+        return userRepo.save(existingUser);
+    }
+
+    public String deleteUserById(int id) {
+        userRepo.deleteById(Long.valueOf(id));
+        return "User " + id + " deleted.";
+    }
+
+    public User getUserById(int id) {
+        return userRepo.findById(Long.valueOf(id)).orElse(null);
+    }
+
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+}
